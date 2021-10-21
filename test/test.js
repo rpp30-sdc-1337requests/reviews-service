bf9467ab7740e07923c298929490de5b5e3988a7
rainealexander <raine@rainealexander.dev>
2021-10-20 23:39:49 -0500

    Revert "Initial database work and testing"
    This reverts commit cae9de3881e556ccf3977f448519322211121af3.

--- test/test.js
+++ test/test.js
@@ -2,11 +2,6 @@ const chai = require('chai');
 const expect = chai.expect;
 const sinon = require('sinon');
 
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-
-const fs = require('fs/promises');
-
 function once(func) {
   let result;
   let called = false;
@@ -30,32 +25,3 @@ describe('Testing testing', function() {
     expect(callback.callCount).to.equal(1);
   });
 });
-
-describe('Testing MongoDB connection', function() {
-  before( function(done) {
-    mongoose.connect('mongodb://localhost:27017/reviewdb', done);
-  });
-  it('should connect to mongodb through mongoose', function() {
-    const CONNECTED = 1;
-    expect(mongoose.connection.readyState).to.equal(CONNECTED);
-  });
-});
-
-describe('Test CSV file operations', function() {
-  it('should open a file from disc', function() {
-    const filename = 'test/sample.csv';
-    let fileOpen = false;
-    fs.open(filename).then((data) => {
-      if (data) {
-        fileOpen = true;
-      }
-    }).catch((error) => {
-      console.error(error);
-    }).then(() => {
-      expect(fileOpen).to.equal(true);
-    });
-  });
-  xit('should transform csv to JSON object', function() {
-
-  });
-})
