db3f6d7e41f47ff3310ca602833fdcb0e8dfa16a
rainealexander <raine@rainealexander.dev>
2021-11-13 15:04:31 -0600

    Final service deployment prep

--- server/app.js
+++ server/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+
+require('dotenv').config();
+
 const bodyparser = require('body-parser');
 const db = require('../database/database.js');
 
